03bc3d269d53f01857488b0d2aa677a6c58236e1
Jenke Scheen <jenke.scheen@choderalab.org>
2024-10-28 21:52:51 -0400

    adds four more replicates to MST expt

--- inputs/01_submit_all.sh
+++ inputs/01_submit_all.sh
@@ -1,22 +1,28 @@
 # simply loop through all production folders while submitting with proper project names.
 # do the bespokefit ones first.
-cd ./bespokefit_benchmarks
-echo "Submitting Bespoke.."
-for target in *; do
-    cd $target
-    echo $target
-    asap-alchemy submit -c public -p benchmark_${target}_bespoke
-    cd ../
-done
+topdir=$(pwd)
+for i in {1..5}; do
+    echo "REPLICATE $i"
+    cd ./bespokefit_benchmarks_$i
+    echo "Submitting Bespoke.."
+    for target in *; do
+        cd $target
+        echo $target
+        asap-alchemy submit -c public -p benchmark_${target}_bespoke_$i
+        cd ../
+    done
 
-# then do the non-bespoke, default ones.
-cd ../
-cd ./default_benchmarks
-echo "Submitting Default.."
-for target in *; do
-    cd $target
-    echo $target
-    asap-alchemy submit -c public -p benchmark_${target}_default
+    # then do the non-bespoke, default ones.
     cd ../
+    cd ./default_benchmarks_$i
+    echo "Submitting Default.."
+    for target in *; do
+        cd $target
+        echo $target
+        asap-alchemy submit -c public -p benchmark_${target}_default_$i
+        cd ../
+    done
+    echo "Done"
+
+    cd $topdir
 done
-echo "Done"
--- inputs/02_gather_all.sh
+++ inputs/02_gather_all.sh
@@ -1,22 +1,28 @@
-# simply loop through all production folders while gathering.
+# simply loop through all production folders while submitting with proper project names.
 # do the bespokefit ones first.
-cd ./bespokefit_benchmarks
-echo "Gathering Bespoke.."
-for target in *; do
-    cd $target
-    echo $target
-    asap-alchemy gather
-    cd ../
-done
+topdir=$(pwd)
+for i in {2..5}; do
+    echo "REPLICATE $i"
+    cd ./bespokefit_benchmarks_$i
+    echo "Gathering Bespoke.."
+    for target in *; do
+        cd $target
+        echo $target
+        asap-alchemy gather
+        cd ../
+    done
 
-# then do the non-bespoke, default ones.
-cd ../
-cd ./default_benchmarks
-echo "Gathering Default.."
-for target in *; do
-    cd $target
-    echo $target
-    asap-alchemy gather
+    # then do the non-bespoke, default ones.
     cd ../
+    cd ./default_benchmarks_$i
+    echo "Gathering Default.."
+    for target in *; do
+        cd $target
+        echo $target
+        asap-alchemy gather
+        cd ../
+    done
+    echo "Done"
+
+    cd $topdir
 done
-echo "Done"
